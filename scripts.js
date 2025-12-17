1c308597ddbee9da83059443c89a5a0c563e1742
KenzieCool62 <kenzie.cool16.2@gmail.com>
2025-12-17 21:48:58 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function goto(link) {
+      window.location.href=link
+}
+
+function show(id) {
+    document.getElementById(id).style.display='block'
+}
+
+function hide(id) {
+    document.getElementById(id).style.display='none'
+}
+
+const pw = "ALLISLEFTWASHOLMES"
